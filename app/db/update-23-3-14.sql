129ba4b1faa3308c90661d8e54fe7c512c7b46f5
Ἡράκλειτος <84381664+Heraclitus-Twin@users.noreply.github.com>
2023-03-24 00:04:03 +0800

    update db

--- app/db/update-23-3-14.sql
+++ app/db/update-23-3-14.sql
@@ -7,3 +7,22 @@ alter table ton_collection_metadata
 alter table ton_collection_metadata
     add deployed bool default false not null;
 
+alter table accept_referral
+    modify addr varchar(64) not null;
+alter table bind_addr
+    modify addr varchar(64) not null;
+
+alter table favorite
+    modify addr varchar(64) not null;
+alter table favorite
+    modify contract varchar(64) not null;
+alter table nft_reg
+    modify contract varchar(64) not null;
+
+alter table proposal
+    modify creator varchar(64) not null;
+
+alter table proposer_white_list
+    modify proposer varchar(64) not null;
+alter table voter
+    modify voter varchar(64) not null;
